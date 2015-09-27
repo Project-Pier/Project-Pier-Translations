33e866f936ad0c48ec13a2f65e694d4a170c7e6b
web3d <web3d@live.com>
2015-09-28 01:44:16 +0800

    all of the plugin lang file are translated

---
+++ application/plugins/tags/language/zh_cn/tags.php
@@ -0,0 +1,16 @@
+<?php
+
+  return array(
+
+    'tag' => '标签',
+    'tags' => '标签',
+
+    'tags used on projects' => '该项目中使用的标签',
+    'number of tagged objects' => '%s个对象',
+    'total objects tagged with' => '该项目中有<b>%s</b>个对象被打上了标签：<b>%s</b>',
+    'no objects tagged with' => '该项目中尚未对象被打上<b>%s</b>标签',
+    'tags widget description' => '以逗号分隔的标签列表 (如: "interface, javascript, public beta")',
+    
+  );
+
+?>
---
+++ application/plugins/time/language/zh_cn/time.php
@@ -0,0 +1,61 @@
+<?php
+
+  return array(
+    // source: actions.php
+
+    // Time
+    'add time' => '添加工时',
+    'edit time' => '编辑工时',
+    'delete time' => '删除工时',
+    'mark as' => '标记为',
+
+    // source: administration.php
+
+    // ---------------------------------------------------
+    //  Time Manager
+    // ---------------------------------------------------
+  
+    'time manager' => '工时管理者',
+    'unbilled time' => '不计费工时',
+    'billed time' => '计费工时',
+    'view by user' => '按用户查看',
+    'view by project' => '按项目查看',
+    'view time by project' => '按项目查看工时',
+    'view time by user' => '按用户查看工时',
+    'unbilled' => '不计费的',
+    'billed' => '计费的',
+    'mark as billed' => '标记为计费的',
+    'mark as unbilled' => '标记为不计费的',
+
+    // source: messages.php
+
+    'no time records in project' => '该项目无工时记录',
+    'confirm delete time' => '你确定要删除该工时记录?',
+
+    // Add time form 
+    'time name required' => '工时名称必填',
+    'time date required' => '工时日期必填',
+    'time hours required' => '工时时长必填',
+
+    // source: objects.php
+  
+    'time' => '工时',
+    'times' => '工时',
+
+    'billable time' => '可计费工时',
+    'bill' => '账单',
+
+    // source: project_interface.php
+
+    'private time desc' => '私有工时仅对所有者公司成员可见，客户公司成员无法看到',
+
+    // Time
+    'report by task' => '由任务报告',
+    'time spent on project' => '项目上耗费的工时',
+    'hour(s)' => '个小时',
+    'times error changing status' => '在变更其中一项时发生错误',
+    'times items successfully marked' => '项目被成功标记为%s',
+    'times time spent on task' => '在任务%s上耗费的工时',
+  );
+
+?>
